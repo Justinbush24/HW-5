d576cfe9e21b7f417065e471acb2349bce57e611
Justin Bush <69733300+Justinbush24@users.noreply.github.com>
2021-04-11 11:30:12 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+
+
+// add current date to top of pageS
+var m = document.querySelector("#currentDate");
+m.textContent = moment().format('dddd[,] MMMM Do YYYY,  h:mm:ss a');
+
+
+
+
+
+// color code the past,present,and future times
+ 
+
+
+console.log("you are connected");
